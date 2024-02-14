32babe91e6a25695cefc40a395e67e00f3650c69
AlbertoCuadra <acuadra@ing.uc3m.es>
2024-02-14 15:16:56 +0100

    Solve: error website macOS

--- utils/website_CT.m
+++ utils/website_CT.m
@@ -1,5 +1,11 @@
 function website_CT()
     % Open Combustion Toolbox's website in default web browser
+    website = 'https://combustion-toolbox-website.readthedocs.io/';
 
-    system('start https://combustion-toolbox-website.readthedocs.io/');
+    if ispc
+        system(['start ', website]);
+        return
+    end
+
+    web(website);
 end
